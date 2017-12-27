52ede0eb3b5b1f29bfc6cd4e35649a22333ce3d6
DariaDesiderata <daria.calwell@gmail.com>
2017-12-26 18:21:16 -0700

    change IP address again

--- server/index.js
+++ server/index.js
@@ -4,7 +4,7 @@ import { Nuxt, Builder } from 'nuxt';
 import api from './api';
 
 const app = express();
-const host = process.env.HOST || '0.0.0.0';
+const host = process.env.HOST || '172.17.0.2';
 const port = process.env.PORT || 3000;
 
 app.set('port', port);

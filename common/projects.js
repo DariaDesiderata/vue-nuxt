2fc4fbb7ccb00d15ab9f299e186d5a81f21067bb
DariaDesiderata <daria.calwell@gmail.com>
2018-10-17 18:58:30 -0600

    added details for 2 projects

--- common/projects.js
+++ common/projects.js
@@ -9,7 +9,7 @@ const projects = [
 Working on this project allowed me to learn various aspects of WebGL, manipulating objects\
 on Canvas and mapping data to visual display of objects.',
     imgUrl: './assets/wanderers_logo.png',
-    projectUrl: 'https://github.com/DariaDesiderata/wanderers'
+    projectUrl: 'https://github.com/DariaDesiderata/wanderers',
   },
   {
     id: 2,
@@ -21,24 +21,30 @@ on Canvas and mapping data to visual display of objects.',
     to provide online shoppers with various filtering, sorting and viewing options for the specific products at different stores across the US and Canada.\
     Other tech used in the project are REST API, GraphQL/Apollo, Scss/Css/Keyframe animations, Webpack/Babel',
     imgUrl: './assets/baker_shop.png',
-    projectUrl: 'https://app.trybaker.com/location'
+    projectUrl: 'https://app.trybaker.com/location',
   },
   {
     id: 3,
-    title: 'Test',
-    description: 'Test',
-    descriptionBody: 'Test',
-    imgUrl: './assets/bw_trees.jpg',
-    projectUrl: 'https://app.trybaker.com/kithchen'
+    title: 'Preferences form',
+    description: 'Complex settings for scheduling marketing campaigns',
+    descriptionBody: `Working on a team of 3 front-end developers, I wrote functional style Javascript to create composable
+       and reusable components to handle complex user input, asynchronous actions (submit, save), loading state, modals 
+       and dynamically updated dialogue windows. Complex state manipulations were handled using a combination or Redux 
+       and Apollo with React for the view layer`,
+    imgUrl: './assets/baker_form.png',
+    projectUrl: 'https://app.trybaker.com/kitchen',
   },
   {
     id: 4,
-    title: 'Test',
-    description: 'Test',
-    descriptionBody: 'Test',
-    imgUrl: './assets/bw_rocks.jpg',
-    projectUrl: 'https://app.trybaker.com/kithchen'
-  }
+    title: 'Analytics dashboard',
+    description: `Dashboard analytics UI detailing each marketing 
+    campaign, analytics for success, in-depth filtering and follow up actions`,
+    descriptionBody: `Solo front-end developer responsible for architecture, implementation, writing tests and adding components to 
+      Storybook for future use. Project involved creating charts, displaying a table with multiple campaigns per month, 
+      creating and updating a calendar, and adding animations for smooth user experience `,
+    imgUrl: './assets/sample_dash.png',
+    projectUrl: 'https://app.trybaker.com/kithchen',
+  },
 ];
 
 export default projects;
--- pages/projects.vue
+++ pages/projects.vue
@@ -2,7 +2,11 @@
   <main>
     <div class="landing-inner">
     <top-nav class="nav"/>
-    <section v-for="project in projects"  v-if='project.id === currentProject.id' class="section">
+    <section 
+      v-for="project in projects" 
+      :key="project.id" 
+      v-if='project.id === currentProject.id' 
+      class="section">
   		<div  class="section__content">
   			<h3 class="section__title">{{ project.title }}</h3>
   			<h5 class="section__description">
@@ -13,7 +17,7 @@
         </p>
   		</div>
   		<div class="section__img">
-  			<img :src="project.imgUrl" class="section__img-inner"></img>
+  			<img :src="project.imgUrl" class="section__img-inner"/>
         <div class="section__more">
     				<a :href="project.projectUrl" class="section__more-link">Explore on Github</a>
             <button @click="showNext(project)" class="section__more-button">See next</button>
@@ -30,42 +34,41 @@ import TopNav from '~/components/Nav.vue';
 import projects from '~/common/projects';
 export default {
   components: {
-    TopNav
+    TopNav,
   },
   data() {
     return {
       currentProject: projects[0],
-      projects: projects
+      projects: projects,
     };
   },
   methods: {
     showNext: function(currentProject) {
-      const index = this.$data.projects.findIndex(
+      const index = this.projects.findIndex(
         project => project.id === currentProject.id
       );
-      if (index < this.$data.projects.length - 1) {
-        this.$data.currentProject = projects[index + 1];
+      if (index < this.projects.length - 1) {
+        this.currentProject = projects[index + 1];
       } else {
-        this.$data.currentProject = projects[0];
+        this.currentProject = projects[0];
       }
-    }
-  }
+    },
+  },
 };
 </script>
 
 <style scoped>
-
 .section__title {
-	font-size: 45px;
-	margin: 0;
-	position: relative;
+  font-size: 45px;
+  margin: 0;
+  position: relative;
 }
 
 .section__description {
   margin: 10px 0;
-	overflow: hidden;
-	position: relative;
-	font-weight: 300;
+  overflow: hidden;
+  position: relative;
+  font-weight: 300;
   font-family: 'Alegreya Sans';
   font-size: 20px;
   background-color: hsla(0, 0%, 98%, 0.68);
@@ -83,11 +86,14 @@ export default {
   z-index: 5;
   position: relative;
 }
+.section__img-inner {
+  height: 397px;
+}
 
 .section__img img {
   filter: grayscale(60%);
   width: 90%;
-  display:block;
+  display: block;
   margin: 5% auto;
   /*transition: transform 0.35s;
 	-webkit-transform: translate3d(-30px,0,0) scale(1.12);
@@ -95,10 +101,10 @@ export default {
 	-webkit-backface-visibility: hidden;
 	backface-visibility: hidden;*/
 }
-.section__img:hover img{
+.section__img:hover img {
   filter: none;
-	-webkit-transform: translate3d(0,0,0) scale(1);
-	transform: translate3d(0,0,0) scale(1);
+  -webkit-transform: translate3d(0, 0, 0) scale(1);
+  transform: translate3d(0, 0, 0) scale(1);
 }
 
 .section__project_id {
@@ -118,50 +124,48 @@ export default {
   padding: 5px 10px;
   background-color: hsla(13, 100%, 95%, 0.81);
   text-decoration: none;
-	cursor: pointer;
+  cursor: pointer;
   color: inherit;
-
 }
 .section__more-button {
   padding: 5px 10px;
   background-color: hsla(13, 100%, 95%, 0.81);
   border: none;
   text-decoration: none;
-	cursor: pointer;
+  cursor: pointer;
 }
 .section__more-link:hover {
-  color: hsla(308, 52%, 59%, .9);
+  color: hsla(308, 52%, 59%, 0.9);
 }
 @media screen and (min-device-width: 1200px) and (max-device-width: 1600px) {
   main {
     background-image: url('../static/assets/bw_opacity10.jpg');
     background-size: cover;
-	  height: 100vh;
-		display: grid;
-		grid-template-rows: 10% 90%;
-		grid-template-areas: 	"header-nav" "section"
-}
+    height: 100vh;
+    display: grid;
+    grid-template-rows: 10% 90%;
+    grid-template-areas: 'header-nav' 'section';
+  }
   .nav {
     grid-area: header-nav;
   }
   .section {
     height: 100%;
-		grid-area: section;
-		display: grid;
-		grid-template-columns: 40% 10% 30% 10% 10%;
+    grid-area: section;
+    display: grid;
+    grid-template-columns: 40% 10% 30% 10% 10%;
     grid-template-rows: 10% 25% 50% auto;
-		grid-template-areas:
-                ". . . ."
-                ". slide-image slide-image slide-image"
-                "slide-content  slide-image slide-image slide-image"
-
-	}
-	.section__content {
-		padding-left: 50px;
+    grid-template-areas:
+      '. . . .'
+      '. slide-image slide-image slide-image'
+      'slide-content  slide-image slide-image slide-image';
+  }
+  .section__content {
+    padding-left: 50px;
     width: 120%;
-		grid-area: slide-content;
+    grid-area: slide-content;
     z-index: 50;
-	}
+  }
   .section__description-text {
     font-family: 'Alegreya Sans';
     line-height: 1.5em;
@@ -170,19 +174,16 @@ export default {
     padding: 20px 0 20px 20px;
   }
 
-	.section__img{
+  .section__img {
     height: auto;
-		grid-area: slide-image;
-		overflow: hidden;
-	}
+    grid-area: slide-image;
+    overflow: hidden;
+  }
   .section__project_id {
     grid-area: section-id;
   }
   .section__more {
     grid-area: more;
   }
-
 }
-
-
 </style>
